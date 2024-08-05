2ae816a07dd6983a0f46e57bf6531eb88f6336da
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-08-05 17:12:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.title("Tarea 2: Estadística para la investigación")
 
 # Definir los enlaces a los que se redirigirá
-enlace_1 = "https://www.example1.com"
-enlace_2 = "https://www.example2.com"
+Tarea_2 = "https://tarea-2-epi-fvyj9oahajx88jhxiunm9g.streamlit.app/"
+Tarea_2_1 = "https://www.example2.com"
 
 # Función para redirigir al usuario
 def redirigir(url):
@@ -13,7 +13,7 @@ def redirigir(url):
 
 # Crear botones y redirigir al pulsarlos
 if st.button("Ir a Tarea 2"):
-    redirigir("Tarea 2")
+    redirigir("Tarea_2")
 
 if st.button("Ir a Tarea 2.1"):
-    redirigir("Tarea 2.1")
+    redirigir("Tarea_2_1")
