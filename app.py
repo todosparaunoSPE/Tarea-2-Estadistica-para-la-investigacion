7e373d8d4182993c5b195fe0f9c4719a9a372550
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-08-05 17:16:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
 # Configurar la página
-st.title("Tarea 2: Estadística para la investigación")
+st.title("Redirección con Botones")
 
 # Definir los enlaces a los que se redirigirá
-Tarea_2 = "https://tarea-2-epi-fvyj9oahajx88jhxiunm9g.streamlit.app/"
-Tarea_2_1 = "https://www.example2.com"
+enlace_1 = "https://tarea-2-epi-fvyj9oahajx88jhxiunm9g.streamlit.app/"
+enlace_2 = "https://www.example2.com"
 
 # Función para redirigir al usuario
 def redirigir(url):
     st.markdown(f'<meta http-equiv="refresh" content="0; url={url}">', unsafe_allow_html=True)
 
 # Crear botones y redirigir al pulsarlos
-if st.button("Ir a Tarea 2"):
-    redirigir("Tarea_2")
+if st.button("Ir a Enlace 1"):
+    redirigir(enlace_1)
 
-if st.button("Ir a Tarea 2.1"):
-    redirigir("Tarea_2_1")
+if st.button("Ir a Enlace 2"):
+    redirigir(enlace_2)
